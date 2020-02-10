8a43e5136902eae6162760bccfefcdfa01151866
crossa9 <56110695+crossa9@users.noreply.github.com>
2020-02-10 11:42:24 +0000

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+
+{
+    string command;
+
+    while(1 ==1) // temporary ondition for now...
+    {
+        command.clear();
+        cout << "What shall i do? ";
+        getline(cin, command);
+        cout << "Your raw command was" << command << end1;
+    }
+    return 0;
+}
